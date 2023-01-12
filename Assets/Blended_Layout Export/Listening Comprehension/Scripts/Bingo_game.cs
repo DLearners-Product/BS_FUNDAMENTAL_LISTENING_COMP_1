0780adf26ba365dced88da88d683b33a5ce44962
Avinashilingam <avinash@dlearners.in>
2023-01-12 16:38:00 +0530

    commit 3
    made all the changes to the bingo game

--- Assets/Blended_Layout Export/Listening Comprehension/Scripts/Bingo_game.cs
+++ Assets/Blended_Layout Export/Listening Comprehension/Scripts/Bingo_game.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class Bingo_game : MonoBehaviour
 {
     public GameObject[] GA_Questions;
     public int Q_count,Ans_count;
-    public GameObject G_selected,G_Finalscreen,G_next;
+    public GameObject G_selected,G_Finalscreen;
+    public TextMeshProUGUI TXT_Max, TXT_Current;
+    public Button backButton;
+    public Button nextButton;
     // Start is called before the first frame update
     void Start()
     {
         G_Finalscreen.SetActive(false);
         Q_count = 0;
-        G_next.SetActive(true);
+        nextButton.gameObject.SetActive(true);
+        backButton.gameObject.SetActive(false);
+        TXT_Max.text = GA_Questions.Length.ToString();
+        int j = Q_count + 1;
+        TXT_Current.text = j.ToString(); 
         for (int i = 0; i < GA_Questions.Length; i++)
         {
             GA_Questions[i].SetActive(false);
@@ -27,6 +35,7 @@ public class Bingo_game : MonoBehaviour
             GA_Questions[Q_count].transform.GetChild(i).gameObject.transform.GetChild(0).gameObject.SetActive(false);
 
         }
+       
     }
 
     // Update is called once per frame
@@ -44,8 +53,10 @@ public class Bingo_game : MonoBehaviour
     {
         Ans_count = 0;
         Q_count ++;
-        if (Q_count < 4)
+        if (Q_count < GA_Questions.Length)
         {
+            int j = Q_count + 1;
+            TXT_Current.text = j.ToString();
             for (int i = 0; i < GA_Questions.Length; i++)
             {
                 GA_Questions[i].SetActive(false);
@@ -56,15 +67,67 @@ public class Bingo_game : MonoBehaviour
             {
                 GA_Questions[Q_count].transform.GetChild(i).gameObject.transform.GetChild(0).gameObject.SetActive(false);
             }
+            BUT_Enabler();
         }
-        else
+       
+       /*  else
         {
             G_Finalscreen.SetActive(true);
             for (int i = 0; i < GA_Questions.Length; i++)
             {
                 GA_Questions[i].SetActive(false);
             }
-            G_next.SetActive(false);
+            nextButton.gameObject.SetActive(false);
+        }*/
+    }
+
+    public void BUT_back()
+    {
+        Ans_count = 20;
+        Q_count--;
+        if (Q_count >= 0)
+        {
+            int j = Q_count + 1;
+            TXT_Current.text = j.ToString();
+            for (int i = 0; i < GA_Questions.Length; i++)
+            {
+                GA_Questions[i].SetActive(false);
+            }
+            GA_Questions[Q_count].SetActive(true);
+
+            for (int i = 0; i < GA_Questions[Q_count].transform.childCount; i++)
+            {
+                GA_Questions[Q_count].transform.GetChild(i).gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            }
+            BUT_Enabler();
+        }
+
+        /*  else
+        {
+            G_Finalscreen.SetActive(true);
+            for (int i = 0; i < GA_Questions.Length; i++)
+            {
+                GA_Questions[i].SetActive(false);
+            }
+            backButton.gameObject.SetActive(false);
+        }*/
+
+    }
+
+    public void BUT_Enabler()
+    {
+        if (Q_count == 0)
+        {
+            backButton.gameObject.SetActive(false);
+        }
+        else if (Q_count == GA_Questions.Length - 1)
+        {
+            nextButton.gameObject.SetActive(false);
+        }
+        else
+        {
+            backButton.gameObject.SetActive(true);
+            nextButton.gameObject.SetActive(true);
         }
     }
 }
